4f343e812fbd8cb3e45405fa13c22ffaca391972
declanqc <72388492+declanqc@users.noreply.github.com>
2022-04-08 01:40:25 +0100

    added prototype scene

---
+++ MusiQuest/Assets/Scripts/BeatScroller.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatScroller : MonoBehaviour
+{
+    public float beatTempo;
+    public bool hasStarted;
+
+
+
+    public void Start()
+    {
+        beatTempo = beatTempo / 60f;
+    }
+
+
+
+    public void Update()
+    {
+        if(!hasStarted)
+        {
+            if(Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }
+
+        }
+        else
+        {
+            transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+        }
+    }
+}
